5b532fddd8a42604bc5e90171c704b4f5383c683
parfait <148047653+pevansfan@users.noreply.github.com>
2024-03-14 19:17:25 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function showOriginal() {
+    let containerImg = document.querySelector(".container-img")
+    let containerImg2 = document.querySelector(".container-img2")
+    containerImg.style.display = 'none'
+    containerImg2.style.display = 'flex'
+}
+
+function showLowPoly() {
+    let containerImg = document.querySelector(".container-img")
+    let containerImg2 = document.querySelector(".container-img2")
+    containerImg.style.display = 'flex'
+    containerImg2.style.display = 'none'
+}
